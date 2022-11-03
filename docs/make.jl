84c1fbbce5a69367158d6140bc20bc24a5572aa4
Haley Kottler <hcolgate@wisc.edu>
2022-11-03 14:24:48 -0500

    deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,8 +3,8 @@ using Documenter, GMMParameterEstimation, HomotopyContinuation
 
 makedocs(sitename="GMMParameterEstimation.jl")
 
-# deploydocs(
-#     repo = "github.com/HaleyColgateKottler/GMMParameterEstimation.jl.git",
-#     versions = nothing,
-#     branch = "gh-pages"
-# )
+deploydocs(
+    repo = "github.com/HaleyColgateKottler/GMMParameterEstimation.jl.git",
+    versions = nothing,
+    branch = "gh-pages"
+)
