b65a59cd70d07ecce8453fcc0ea9e6221aa10c76
Haley Kottler <hcolgate@wisc.edu>
2022-11-16 15:14:54 -0600

    update documenter yml

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,4 @@ makedocs(sitename="GMMParameterEstimation.jl",
 
 deploydocs(
     repo = "github.com/HaleyColgateKottler/GMMParameterEstimation.jl.git",
-    versions = nothing,
-    branch = "gh-pages"
 )
